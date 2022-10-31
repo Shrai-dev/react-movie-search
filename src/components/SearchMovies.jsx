871aae8076babd02bdb392f3ab261402ce51a79b
Anastasia <nastia.shrai@gmail.com>
2022-10-31 22:32:06 +0200

    feat: add rendering list of movies

--- src/components/SearchMovies.jsx
+++ src/components/SearchMovies.jsx
@@ -1,37 +1,45 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { MovieCard } from './MovieCard';
 
 export const SearchMovies = () => {
-	const [query, setQuery] = useState("");
-	const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
 
-	const searchMovies = async (e) => {
-		e.preventDefault();
-		const url = `https://api.themoviedb.org/3/search/movie?api_key=1522de262832f8a37e8d96e7ed928ba5&language=en-US&query=${query}&page=1&include_adult=false`;
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=1522de262832f8a37e8d96e7ed928ba5&language=en-US&query=${query}&page=1&include_adult=false`;
 
-		try {
-			const res = await fetch(url);
-			const data = await res.json();
-			setMovies(data.results);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	return (
-		<>
-			<form className='form'>
-				<input
-					className='input'
-					type='text'
-					name='query'
-					placeholder='Enter movie name...'
-					value={query}
-					onChange={(e) => setQuery(e.target.value)}
-				/>
-				<button className='button' type='submit' onClick={searchMovies}>
-					Search
-				</button>
-			</form>
-		</>
-	);
+  return (
+    <>
+      <form className='form'>
+        <input
+          className='input'
+          type='text'
+          name='query'
+          placeholder='Enter movie name...'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className='button' type='submit' onClick={searchMovies}>
+          Search
+        </button>
+      </form>
+      <div className='movies-list'>
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
+  );
 };
